deb8e0c123771ef10d8ecd9c89271fc20e7f7992
Maxi <maxitreggia@gmail.com>
2024-04-18 22:11:17 -0300

    terminar ejercico 5

--- practica2/ejercicio5/ejercicio5.py
+++ practica2/ejercicio5/ejercicio5.py
@@ -4,9 +4,18 @@
 
 cities = int(input("Ingrese la cantidad de ciudades: "))
 list_cities = []
+keep = ''
 
 for i in range(cities):
     list_cities.append(input(f"Ingrese la ciudad{i + 1}/{cities}: ").upper())
 
 
 print(f"La lista de ciudades es: {list_cities}")
+
+while keep != 'N':
+    search_cities = input("Ingrese una ciudad que desea buscar: ")
+    if search_cities in list_cities:
+        print(f"La lista de ciudades es: {list_cities}")
+    else:
+        print("La ciudad ingresada no se encuntra en la lista")
+    keep = input("Desea continuar? [S/N] ").upper()
